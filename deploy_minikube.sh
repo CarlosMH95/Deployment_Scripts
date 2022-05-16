7140fdc124cf9a6f8404ace39731edb8a25729e2
Carlos Manosalvas <cmanosalvas95@gmail.com>
2022-05-16 12:13:40 -0500

    Update deploy_minikube.sh

--- deploy_minikube.sh
+++ deploy_minikube.sh
@@ -6,24 +6,16 @@
 # El siguiente es la direccion y nombre del configmap. 
 # El ultimo parametro se refiere a la direccion y nombre del archivo de deployment.
 echo 'Empieza el deployment'
-echo
 echo 'Creando Imagen Docker'
-echo
 cd $1
 docker build -t $2 .
 cd ..
-
 echo 'Imagen Creada'
-echo
 echo 'Cargando imagen a Minikube'
-echo
 minikube image load $2
 
 echo 'Imagen Cargada'
-echo
-
 echo 'Cargando Configmaps y Deployments'
-echo
 kubectl apply -f $3
 kubectl apply -f $4
 echo 'Fin.'
